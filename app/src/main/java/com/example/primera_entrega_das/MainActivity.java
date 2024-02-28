fc5119012afb07b880b5603bfa17874438b815e0
mikelp <mikel.pedrolinaje@gmail.com>
2024-02-28 18:50:45 +0100

    Toolbar hecha

--- app/src/main/java/com/example/primera_entrega_das/MainActivity.java
+++ app/src/main/java/com/example/primera_entrega_das/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.primera_entrega_das;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.labarra);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_labarra,menu);
+        return true;
     }
 }
