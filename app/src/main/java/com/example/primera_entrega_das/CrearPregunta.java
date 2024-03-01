18d0277c558a08239651a0bfa27926f1933d6051
mikelp <mikel.pedrolinaje@gmail.com>
2024-03-01 20:46:02 +0100

    Comienzo del diseño de la pantalla de CrearPregunta (sin BD)

---
+++ app/src/main/java/com/example/primera_entrega_das/CrearPregunta.java
@@ -0,0 +1,14 @@
+package com.example.primera_entrega_das;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearPregunta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_pregunta);
+    }
+}
