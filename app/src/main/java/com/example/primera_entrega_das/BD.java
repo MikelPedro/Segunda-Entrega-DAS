302ea44b469c396cf6f01bea20d1fd27fdc28ad1
mikelp <mikel.pedrolinaje@gmail.com>
2024-03-02 15:02:39 +0100

    Comienzo de la BD

--- app/src/main/java/com/example/primera_entrega_das/BD.java
+++ app/src/main/java/com/example/primera_entrega_das/BD.java
@@ -1,4 +1,25 @@
 package com.example.primera_entrega_das;
 
-public class BD {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BD extends SQLiteOpenHelper {
+
+
+    public BD(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
