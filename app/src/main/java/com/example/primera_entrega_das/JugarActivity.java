26fb36f7cbc85cb9b3201c66153da843ed45b112
mikelp <mikel.pedrolinaje@gmail.com>
2024-03-05 16:40:42 +0100

    Fragment añadido en JuegoActivity

--- app/src/main/java/com/example/primera_entrega_das/JugarActivity.java
+++ app/src/main/java/com/example/primera_entrega_das/JugarActivity.java
@@ -1,27 +1,66 @@
 package com.example.primera_entrega_das;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
 
+
 public class JugarActivity extends AppCompatActivity {
+
     private ArrayList<ModeloPregunta> listaPreguntasTemas;
+
+    public static class HorizontalFragment extends Fragment {
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.activity_jugar_landscape, container, false);
+            return v;
+        }
+    }
+
+    public static class VerticalFragment extends Fragment {
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.activity_jugar_portrait, container, false);
+            return v;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jugar);
+        super.setContentView(R.layout.activity_jugar);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Cargar el Fragmento para pantallas grandes en modo horizontal
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.elFragmento, new HorizontalFragment())
+                    .commit();
+        } else {
+
+            // Cargar el Fragmento para pantallas pequeñas en modo vertical
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.elFragmento, new VerticalFragment())
+                    .commit();
+        }
 
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String tema = extras.getString("TemaJugar");
 
-
+            /*
             OperacionesBD bd = new OperacionesBD(this,1);
             listaPreguntasTemas = bd.obtenerPreguntaTema(tema);
 
@@ -44,8 +83,21 @@ public class JugarActivity extends AppCompatActivity {
             // Habria que recorrer todas las preguntas, seleccionar una de manera aleatoria y mirar
             //la correcta
             //Guardar Pregunta en un fichero ?
+
+             */
         }
 
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        View frag = super.getSupportFragmentManager().findFragmentById(R.id.elFragmento).getView();
+
+        //puedes dar estilos, buscar botones, textviews etc
+        //bajar aqui lo de buscar para meter el texto en la checkbox
+    }
+
 }
