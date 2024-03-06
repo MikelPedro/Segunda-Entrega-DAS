c60ec726bbca4fd26d2f427cc0f1e041e7b2f04b
mikelp <mikel.pedrolinaje@gmail.com>
2024-03-06 01:05:39 +0100

    Fragment con botones arreglado y avance en JugarActivity

--- app/src/main/java/com/example/primera_entrega_das/JugarActivity.java
+++ app/src/main/java/com/example/primera_entrega_das/JugarActivity.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,8 +21,9 @@ import java.util.Random;
 
 public class JugarActivity extends AppCompatActivity {
 
-    private ArrayList<ModeloPregunta> listaPreguntasTemas;
+    protected ArrayList<ModeloPregunta> listaPreguntasTemas;
 
+    //Parte necesaria para fragments
     public static class HorizontalFragment extends Fragment {
         public View onCreateView(@NonNull LayoutInflater inflater,
                                  @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -43,52 +46,33 @@ public class JugarActivity extends AppCompatActivity {
         super.setContentView(R.layout.activity_jugar);
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            // Cargar el Fragmento para pantallas grandes en modo horizontal
+            // Cargar el Fragmento en modo horizontal
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.elFragmento, new HorizontalFragment())
                     .commit();
         } else {
 
-            // Cargar el Fragmento para pantallas pequeñas en modo vertical
+            // Cargar el Fragment en modo vertical
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.elFragmento, new VerticalFragment())
                     .commit();
         }
 
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String tema = extras.getString("TemaJugar");
-
-            /*
-            OperacionesBD bd = new OperacionesBD(this,1);
-            listaPreguntasTemas = bd.obtenerPreguntaTema(tema);
-
-            // Indice aleatorio para obtener una pregunta aleatoria
-            Random random = new Random();
-            int iAleatorio = random.nextInt(listaPreguntasTemas.size());
-
-            // Manera de obtener la pregunta correspondiente al índice aleatorio
-            ModeloPregunta mPregunta = listaPreguntasTemas.get(iAleatorio);
-
-
-
-            Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getPregunta());
-            Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuesta1());
-            Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuesta2());
-            Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuesta3());
-            Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuestaCorrecta());
-            Log.d("PREGUNTA SOBRE TEMA", String.valueOf(listaPreguntasTemas.size()));
-            //Funciona
-            // Habria que recorrer todas las preguntas, seleccionar una de manera aleatoria y mirar
-            //la correcta
-            //Guardar Pregunta en un fichero ?
-
-             */
-        }
-
+        //Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getPregunta());
+        //Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuesta1());
+        //Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuesta2());
+        //Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuesta3());
+        //Log.d("PREGUNTA SOBRE TEMA",listaPreguntasTemas.get(iAleatorio).getRespuestaCorrecta());
+        //Log.d("PREGUNTA SOBRE TEMA", String.valueOf(listaPreguntasTemas.size()));
+        //Funciona
+        // Habria que recorrer todas las preguntas, seleccionar una de manera aleatoria y mirar
+        //la correcta
+        //Guardar Pregunta en un fichero ?
 
     }
+
+
+
 
     @Override
     protected void onStart() {
@@ -96,8 +80,50 @@ public class JugarActivity extends AppCompatActivity {
 
         View frag = super.getSupportFragmentManager().findFragmentById(R.id.elFragmento).getView();
 
-        //puedes dar estilos, buscar botones, textviews etc
-        //bajar aqui lo de buscar para meter el texto en la checkbox
+        Bundle extras = getIntent().getExtras();
+        if (frag != null) {
+            RadioButton radioButtonR1 = frag.findViewById(R.id.radioButtonJugar1);
+            RadioButton radioButtonR2 = frag.findViewById(R.id.radioButtonJugar2);
+            RadioButton radioButtonR3 = frag.findViewById(R.id.radioButtonJugar3);
+            TextView textViewPregunta = frag.findViewById(R.id.textViewPreguntaJugar);
+            Button btnSigPregunta = frag.findViewById(R.id.botonSigPreg);
+
+            if (extras != null) {
+                String tema = extras.getString("TemaJugar");
+
+
+                OperacionesBD bd = new OperacionesBD(this, 1);
+                listaPreguntasTemas = bd.obtenerPreguntaTema(tema);
+
+                ModeloPregunta mPregunta = this.obtenerPreguntaLista(listaPreguntasTemas);
+                //puedes dar estilos, buscar botones, textviews etc
+                //bajar aqui lo de buscar para meter el texto en la checkbox
+
+                textViewPregunta.setText(mPregunta.getPregunta());
+                radioButtonR1.setText(mPregunta.getRespuesta1());
+                radioButtonR2.setText(mPregunta.getRespuesta2());
+                radioButtonR3.setText(mPregunta.getRespuesta3());
+
+            }
+
+            //me falta que saque preguntas aleatorias de todos los temas
+            // y la verificacion de la pregunta y la noti
+        }
+
+
+
+
     }
+
+    //clase para obtener una pregunta aleatoria dada una lista cualquiera, ya sea en base a un tema o no.
+    public ModeloPregunta obtenerPreguntaLista(ArrayList<ModeloPregunta> listaPreguntas) {
+
+        Random random = new Random();
+        int iAleatorio = random.nextInt(listaPreguntas.size());
+
+        // Devolver la pregunta correspondiente al índice aleatorio
+        return listaPreguntas.get(iAleatorio);
+    }
+
 
 }
