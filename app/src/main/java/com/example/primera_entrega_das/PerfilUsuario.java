29a4c04a7272e68554cac14e55dc3e9424cd469c
mikelp <mikel.pedrolinaje@gmail.com>
2024-04-13 19:16:33 +0200

    pantallas de perfil creadas

--- app/src/main/java/com/example/primera_entrega_das/PerfilUsuario.java
+++ app/src/main/java/com/example/primera_entrega_das/PerfilUsuario.java
@@ -2,7 +2,10 @@ package com.example.primera_entrega_das;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class PerfilUsuario extends AppCompatActivity {
 
@@ -11,4 +14,14 @@ public class PerfilUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
     }
+
+
+    public void OnClickVolverMain(View v){
+        //Al darle click te lleva a la MainActivity
+        Intent volver = new Intent(this,MainActivity.class);
+        this.startActivity(volver);
+        finish();
+    }
+
+
 }
